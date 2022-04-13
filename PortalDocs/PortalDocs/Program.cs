43241a6d0620b6a5efa611d19e4fcbe68aee9d81
Rubens Pires <rubenspires@outlook.com>
2022-04-12 21:48:32 -0300

    teste incluindo console whiteline

--- PortalDocs/PortalDocs/Program.cs
+++ PortalDocs/PortalDocs/Program.cs
@@ -14,6 +14,7 @@ namespace PortalDocs
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            Console.WriteLine();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
